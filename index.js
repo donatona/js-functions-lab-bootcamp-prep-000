6092d7e6387b4921c0b9a45fcf9f0dcd59c0ea3b
Tona Morales-Calkins <tona.mc@gmail.com>
2018-09-22 19:16:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
   return('Happy holidays!');
 }
 function happyHolidaysTo(name) {
-  return(`Happy hoidays, ${name}!`);
+  return(`Happy holidays, ${name}!`);
 }
 
 function happyHolidayTo(holiday, name) {
